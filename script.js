52f2b4113762010112ee9fb32fa3c5e2c1c65d3a
Leandro Rodriguez <leandrorodriguez1130@gmail.com>
2023-09-02 17:25:48 -0300

    javascript

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const restaurante = {
+      nombre: "Lado V",
+      mesasTotales: 20,
+      mesasDisponibles: 20,
+    };
+  
+    function verificarDisponibilidad() {
+      let mensaje = restaurante.nombre + "\n";
+      mensaje += "Mesas totales: " + restaurante.mesasTotales + "\n";
+      mensaje += "Mesas disponibles: " + restaurante.mesasDisponibles + "\n";
+  
+      if (restaurante.mesasDisponibles > 0) {
+        mensaje += "Tenemos mesas disponibles, podes realizar una reserva";
+      } else {
+        mensaje += "No tenemos mesas disponibles para reservar, pero podes venir sin reserva y anotarte en lista de espera";
+      }
+     console.log(mensaje)
+      alert(mensaje);
+    }
+  
+    function simularReservas() {
+      while (restaurante.mesasDisponibles > 0) {
+        restaurante.mesasDisponibles--;
+        console.log("Mesa reservada. Mesas disponibles: " + restaurante.mesasDisponibles);
+      }
+    }
+  
+    // Obtener el botón por su ID y agregar un evento de clic
+    const verificarButton = document.getElementById("verificarButton");
+    verificarButton.addEventListener("click", function () {
+      verificarDisponibilidad();
+      simularReservas();
+      alert("Ha pasado tiempo. Vuelve a verificar la disponibilidad.");
+      console.log ("Ha pasado tiempo. Vuelve a verificar la disponibilidad.");
+    });
+  });
+  
